74f56596b254801aa150698f53ce3456f4c36fa5
MRanjitha <ranjithamylu@gmail.com>
2017-11-13 00:41:56 +0530

    Second commit

--- FirstPro/blog/views.py
+++ FirstPro/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+from .forms import PostForm
 
-# Create your views here.
+def post_list(request):
+	posts = Post.objects.exclude(name=None).values()
+	return render(request, 'blog/Sample.html',{'posts': posts})
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
